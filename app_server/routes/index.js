306c71e3afb486f9b94973ce315b10ba35f450bd
Alejandro Urdiales <alejandrourdialesmanzanaro@gmail.com>
2019-04-04 09:33:55 -0700

    Sesión 03

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
-var ctrlMain = require('../controllers/main');
+var ctrlLocations = require('../controllers/locations');
+var ctrlOthers = require('../controllers/others');
 
+/* Locations pages */
+router.get('/', ctrlLocations.homelist);
+router.get('/location', ctrlLocations.locationInfo);
+router.get('/location/review/new', ctrlLocations.addReview);
 
-/* GET home page. */
-router.get('/', ctrlMain.index);
+/* Other pages */
+router.get('/about', ctrlOthers.about);
 
 module.exports = router;
